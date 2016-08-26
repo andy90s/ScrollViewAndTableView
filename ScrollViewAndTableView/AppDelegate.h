fe2b317a31291c9251690406876703db7c25e16c
梁先华 <909901234@qq.com>
2016-08-26 14:04:29 +0800

    Initial Commit

---
+++ ScrollViewAndTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewAndTableView
+//
+//  Created by 梁先华 on 16/8/26.
+//  Copyright © 2016年 梁先华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewAndTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScrollViewAndTableView
+//
+//  Created by 梁先华 on 16/8/26.
+//  Copyright © 2016年 梁先华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
